bfda8d763ff1bfd7902d062ce419bdb6f8ba89c9
zhuang66da <943117167@qq.com>
2020-12-02 14:15:30 +0800

    01.测试提交到GitHub。

---
+++ src/com/bjpowernode/github/GitHubTest01.java
@@ -0,0 +1,16 @@
+package com.bjpowernode.github;
+
+/**
+ * @author zhuang
+ * @program git
+ * @className GitHubTest01
+ * @description 使用idea工具测试提交到GitHub
+ * @Date 2020/12/1 11:45
+ */
+public class GitHubTest01 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println(i);
+    }
+}
